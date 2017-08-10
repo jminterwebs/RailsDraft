4360948e2bbc4da3b1a226a3f8b6811a23c3dbc4
Unknown <jmverdone@gmail.com>
2017-08-10 19:38:11 -0400

    stubbed out controllers

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,2 +1,27 @@
 class LeaguesController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def update
+  end
+
+  def destroy
+
+  end
+
+
 end
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -4,4 +4,11 @@ class PlayersController < ApplicationController
   def index
     @players = Player.all
   end
+
+
+  def show
+    @player = Player.find(params[:id])
+  end
+
+
 end
