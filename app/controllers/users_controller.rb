6a063eef1b2277255269d853ec2a458165c2fa5a
Unknown <jmverdone@gmail.com>
2017-08-16 22:13:01 -0400

    debuging and templating setups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,21 @@ def index
 end
 
 def show
-
+  @user = User.find(params[:id])
 end
 
 def new
-
+  @user = User.new
 end
 
 def create
+  @user =User.new(user_params)
 
+  if @user.save
+    redirect_to @user
+  else
+    render :new
+  end
 end
 
 def update
@@ -22,6 +28,12 @@ end
 def destroy
 
 end
+
+private
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-  require 'httparty'
+require 'httparty'
 
 
 def player_seed(offset)
