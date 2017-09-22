3f0f97b9925dc96783f0773f66ccdee10ab68dc1
Unknown <jmverdone@gmail.com>
2017-09-21 22:09:58 -0400

    cors fix

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Railsdraft
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :options, :update, :put, :patch]
       end
     end
   end
