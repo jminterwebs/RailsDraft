80fef158801093db2cbc75fe3dd00b87a2723fbc
Unknown <jmverdone@gmail.com>
2017-09-30 14:09:13 -0400

    player serailzer

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,3 +1,6 @@
 class PlayerSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :unique_player_id, :teamAbbr, :rank, :position, :team_id, :projected_points
+
+  has_many :leagues, serializer: LeagueSerializer
+  has_many :teams, serializer: TeamSerializer
 end
