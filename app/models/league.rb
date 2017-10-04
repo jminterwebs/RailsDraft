3742da64a82579c013e168e93418a9f3684f2dd9
Unknown <jmverdone@gmail.com>
2017-10-03 23:18:23 -0400

    acepts nested attr

--- app/models/league.rb
+++ app/models/league.rb
@@ -1,6 +1,11 @@
 class League < ApplicationRecord
   has_many :teams
+  has_many :player_leagues
+  has_many :players, through: :player_leagues
 
-  has_many :player_teams
-  has_many :player, through: :player_teams
+  accepts_nested_attributes_for :teams
+
+
+
+
 end
