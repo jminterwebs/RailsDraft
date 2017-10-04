ff6eba16b95535805e2f3d9379985cc8f189509a
Unknown <jmverdone@gmail.com>
2017-10-03 23:14:38 -0400

    added draft ino

--- db/migrate/20170810161139_create_leagues.rb
+++ db/migrate/20170810161139_create_leagues.rb
@@ -2,7 +2,9 @@ class CreateLeagues < ActiveRecord::Migration[5.0]
   def change
     create_table :leagues do |t|
       t.string :name
-      
+      t.integer :draft_round
+      t.integer :draft_spot
+
       t.timestamps
     end
   end
--- db/migrate/20170810161201_create_teams.rb
+++ db/migrate/20170810161201_create_teams.rb
@@ -3,6 +3,7 @@ class CreateTeams < ActiveRecord::Migration[5.0]
     create_table :teams do |t|
       t.string :name
       t.integer :league_id
+      t.integer :draft_spot
       t.timestamps
     end
   end
