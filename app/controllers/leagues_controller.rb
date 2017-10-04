6fcbc681a083c429c340fa303ccf0be8128cfaf0
Unknown <jmverdone@gmail.com>
2017-10-03 23:17:19 -0400

    create league with teams and players set up

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -1,19 +1,24 @@
 class LeaguesController < ApplicationController
+  before_action :set_league, only: :show
 
   def index
     render json: League.all
   end
 
   def show
-
+    render json: @league
   end
 
   def new
+    @league = League.new
+    @league.build_teams
 
   end
 
   def create
-    
+    @league = League.new(league_params)
+    @league.players << Player.all
+    @league.save
   end
 
   def update
@@ -23,5 +28,14 @@ class LeaguesController < ApplicationController
 
   end
 
+  private
+
+    def league_params
+      params.require(:league).permit(:name, teams_attributes: [:name] )
+    end
+
+    def set_league
+      @league = League.find(params[:id])
+    end
 
 end
