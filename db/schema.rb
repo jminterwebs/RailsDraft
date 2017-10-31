acfa307b55133c10065f37fa6f88adead2bf3511
Unknown <jmverdone@gmail.com>
2017-10-30 20:59:03 -0400

    testing deploy

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170928011441) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "leagues", force: :cascade do |t|
     t.string   "name"
     t.integer  "draft_round", default: 1
