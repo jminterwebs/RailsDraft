a4a8f4d6a67cd567dd06013c230aa9c9202f1388
Unknown <jmverdone@gmail.com>
2017-12-20 20:54:22 -0500

    changed from first and last name to full_name

--- app/serializers/player_serializer.rb
+++ app/serializers/player_serializer.rb
@@ -1,5 +1,5 @@
 class PlayerSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :unique_player_id, :teamAbbr, :rank, :position, :projected_points
+  attributes :id, :full_name, :unique_player_id, :teamAbbr, :rank, :position, :projected_points
 
-  
+
 end
